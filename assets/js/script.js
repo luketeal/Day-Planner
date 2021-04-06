7e8c768011c264dd1c49555286226cc66935e86f
Luke Teal <l.teal21@gmail.com>
2021-04-05 22:41:47 -0400

    styled width to be more responsive added placeholder text added delete buttons pseudo coded javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+// jquery selector for all save buttons
+// jquery selector for all text areas - generates an array
+// jquery selector for all delete buttons
 
-
+// load date to header
 $("#currentDay").text(moment().format('MMMM Do, YYYY'));
 
+// jquery event listener for the save button
+// get the value of all the text areas and save to an array and save the array to local storage
+
+// jquery event listener for the delete buttons
+// nav to the closest text area and remove content
+// get the value of all the text areas and save to an array and save the array to local storage
+
+// on page load compare current time with the value of each span
+// if the time has passed, assign background color of next text area to gray
+// if the time is in the future, assign the background color to beg green
+// if the time now, assign the color to be yellow
+
+// considerations - the delete button should be disabled unless there is some thing in the text area.
