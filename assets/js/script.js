a402d4748366d2b62d5970cdde872412e5e5165d
Luke Teal <l.teal21@gmail.com>
2021-04-08 21:34:12 -0400

    Add saveSchedule function and set page load JS Add data-time attributes to each time-slot update text color remove background and border styling of text areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ console.log(saveButtonEl)
 
 // jquery selector for all text areas
 let textAreaEl = $('textarea')
+// let textAreaEl = document.querySelectorAll('textarea')
 
-console.log(textAreaEl)
+console.log(textAreaEl[4].value)
 
 // jquery selector for all delete buttons
 let deleteButtonEl = $('.delete')
@@ -22,6 +23,22 @@ console.log(resetButtonEl)
 $('#currentDay').text(moment().format('MMMM Do, YYYY'));
 
 // On page load: get info in local storage and display it
+let savedSchedule = JSON.parse(localStorage.getItem('savedSchedule'));
+console.log(savedSchedule)
+
+if (savedSchedule != null) {
+    for(let eachTask of savedSchedule) {
+        for(let eachTime of textAreaEl) {
+            console.log(eachTime.getAttribute("data-time"))
+            console.log(eachTask)
+            if(eachTask.time === eachTime.getAttribute("data-time")) {
+                eachTime.value = eachTask.task
+                console.log(eachTime.value)
+                console.log(eachTask.task)
+            }
+        }
+    }
+}
 
 // on page load compare current time with the value of each span
 
@@ -30,35 +47,96 @@ let timeNow = moment().hour() - 8
 console.log(timeNow)
 
 for (let i = 0; i<textAreaEl.length; i++) {
+    // if the time has passed, assign background color of next text area to gray
     if(i<timeNow) {
-        $(textAreaEl[i]).addClass('bg-primary')
+        $(textAreaEl[i]).addClass('past')
+
+    // if the time now, assign the color to be yellow
+    } else if(i===timeNow) {
+        $(textAreaEl[i]).addClass('present')
+
+    // if the time is in the future, assign the background color to red
+    } else {
+        $(textAreaEl[i]).addClass('future')
     }
 }
 
-// if the time has passed, assign background color of next text area to gray
-// if the time is in the future, assign the background color to beg green
-// if the time now, assign the color to be yellow
 
 // Functions
 
 // save array function
-function saveSchedule() {
+function saveSchedule(event) {
 
 // get the value of all the text areas and save to an array and save the array to local storage
 
-// reload page?
+    let savedEvent = {
+        task: event.currentTarget.parentNode.childNodes[3].value,
+        time: event.currentTarget.parentNode.childNodes[3].getAttribute("data-time")
+    }
+
+    console.log(savedEvent.task)
+    console.log(savedEvent.time)
+
+    if (savedSchedule === null) {
+        savedSchedule = [savedEvent];
+        localStorage.setItem('savedSchedule', JSON.stringify(savedSchedule))
+        console.log('you added your first item. it is ' + savedSchedule.task)
+        return
+
+    } else {
+        // let savedSchedule = JSON.parse(localStorage.getItem('savedSchedule'));
+        for(let each of savedSchedule) {
+            if(each.time === savedEvent.time) {
+                each.task = savedEvent.task
+                each.time = savedEvent.time
+
+                localStorage.setItem('savedSchedule', JSON.stringify(savedSchedule))
+                console.log('you replaced an item')
+                return
+            } 
+        }
+
+        savedSchedule.push(savedEvent)
+        localStorage.setItem('savedSchedule', JSON.stringify(savedSchedule))
+        console.log('you added an item to the array')
+        return
+
+    } 
 
 }
 
 
+// reload page?
+
+
+
+// delete item function
+function deleteItem(event) {
+
+    let task = event.target.parentNode.childNodes[3]
+    let time = task.getAttribute("data-time")
+    console.log(task.value)
+    console.log(time)
+
+
+
+// nav to the closest text area and remove content
+
+// call save schedule function
+// saveSchedule()
+
+}
 
 // jquery event listener for the save button
 saveButtonEl.on('click', saveSchedule)
 
 
 
+
 // jquery event listener for the delete buttons
-// nav to the closest text area and remove content
+deleteButtonEl.on('click', deleteItem) 
+
+
 // get the value of all the text areas and save to an array and save the array to local storage
 
 
